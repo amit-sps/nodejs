d4e7e6e2c44310a2768a14e46e8e157b1aad2878
Amit Thakur <amitthakur.softprodigy@gmail.com>
2022-06-02 12:37:31 +0530

    duplex done !

--- streams.js
+++ streams.js
@@ -2,18 +2,16 @@ console.log("Learning Streams.......................");
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-const {PassThrough,Duplex}=require("stream")
+const { PassThrough, Duplex } = require("stream");
 
-class Throttle extends Duplex{
-  constructor(time){
+class Throttle extends Duplex {
+  constructor(time) {
     super();
-    this.delay=time;
+    this.delay = time;
   }
-  _read(){
-
-  }
-   // Writes the data, push and set the delay/timeout
-   _write(chunk, encoding, callback) {
+  _read() {}
+  // Writes the data, push and set the delay/timeout
+  _write(chunk, encoding, callback) {
     this.push(chunk);
     setTimeout(callback, this.delay);
   }
@@ -21,7 +19,7 @@ class Throttle extends Duplex{
     this.push(null);
   }
 }
-const tunnel=new PassThrough()
+const tunnel = new PassThrough();
 const throttle = new Throttle(1000);
 http
   .createServer((req, res) => {
@@ -30,51 +28,54 @@ http
       res.end();
     } else if (req.url === "/data") {
       const readStream = fs.createReadStream(path.join(__dirname, "/demo.txt"));
-      const writeStream=fs.createWriteStream(path.join(__dirname, "/writeDemo.txt"))
-      writeStream.on("drain",()=>{
-        console.log("drain")
-    })
-    writeStream.on("finish",()=>{
-        console.log("finish")
-    })
-    writeStream.on("close",()=>{
-        console.log("writing close!")
-    })
-    writeStream.on("error",(err)=>{
-        console.log("writing error>>>>>>>>>>>>>>>>>")
-        console.log(err)
-    })
-      readStream.on("data",(data)=>{
-          console.log("data reading.......")
-          writeStream.write(data)
-          res.write(data)
-      })
-      readStream.on("end",()=>{
-          console.log("data end!")
-          writeStream.end();
-          writeStream.close();
-          res.end()
-          readStream.close()
-      })
-      readStream.on("error",(err)=>{
-          console.log("________ERROR PART___________")
-          console.log(err)
-      })
-      readStream.on("close",()=>{
-          console.log("*************File Closed!***********")
-      })
-    //   readStream.on("readable",()=>{
-    //       console.log("readble stream")
-    //   })
-    } 
-    else if(req.url==="/duplex"){
+      const writeStream = fs.createWriteStream(
+        path.join(__dirname, "/writeDemo.txt")
+      );
+      writeStream.on("drain", () => {
+        console.log("drain");
+      });
+      writeStream.on("finish", () => {
+        console.log("finish");
+      });
+      writeStream.on("close", () => {
+        console.log("writing close!");
+      });
+      writeStream.on("error", (err) => {
+        console.log("writing error>>>>>>>>>>>>>>>>>");
+        console.log(err);
+      });
+      readStream.on("data", (data) => {
+        console.log("data reading.......");
+        writeStream.write(data);
+        res.write(data);
+      });
+      readStream.on("end", () => {
+        console.log("data end!");
+        writeStream.end();
+        writeStream.close();
+        res.end();
+        readStream.close();
+      });
+      readStream.on("error", (err) => {
+        console.log("________ERROR PART___________");
+        console.log(err);
+      });
+      readStream.on("close", () => {
+        console.log("*************File Closed!***********");
+      });
+      //   readStream.on("readable",()=>{
+      //       console.log("readble stream")
+      //   })
+    } else if (req.url === "/duplex") {
       const readStream = fs.createReadStream(path.join(__dirname, "/demo.txt"));
-      const writeStream=fs.createWriteStream(path.join(__dirname,"duplex.txt"))
-      tunnel.on("data",(chunkData)=>{
-        console.log("chunk data")
-      })
-      readStream.pipe(throttle).pipe(tunnel).pipe(res)
-    }else {
+      const writeStream = fs.createWriteStream(
+        path.join(__dirname, "duplex.txt")
+      );
+      tunnel.on("data", (chunkData) => {
+        console.log("chunk data : "+chunkData);
+      });
+      readStream.pipe(throttle).pipe(tunnel).pipe(res);
+    } else {
       res.write("Invalid Page!");
       res.end();
     }
